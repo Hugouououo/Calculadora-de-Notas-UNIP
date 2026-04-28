86ae5d25a2b95d4dd7f4dd1f5fed36ff878231ee
Hugo <hugomoreiralaprano@gmail.com>
2026-04-27 21:51:44 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+// média
+let nota_1 = document.getElementsByName("nota_1")
+let nota_2 = document.getElementsByName("nota_2")
+let trab = document.getElementsByName("trab")
+const calcular_media = document.getElementsByName("calcular_media")
+// resultado da média
+let media = document.getElementById("media")
+let resultado = document.getElementById("resultado")
+// exame (caso média < 7)
+let nota_exame = document.getElementsByName("nota_exame")
+let trab_exame = document.getElementsByName("trab_exame")
+const calcular_exame = document.getElementsByName("calcular_exame")
+// resultado do exame
+let media_exame = document.getElementsByName("media-exame")
+let resultado_final = document.getElementsByName("resultado-final")
+
