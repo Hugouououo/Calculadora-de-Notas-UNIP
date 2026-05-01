97ee15f250eed3a7fd5ecead80c4e2154d5cf340
Hugo <hugomoreiralaprano@gmail.com>
2026-05-01 14:36:10 -0300

    Preparação

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // média
-let nota_1 = document.getElementsByName("nota_1")
-let nota_2 = document.getElementsByName("nota_2")
-let trab = document.getElementsByName("trab")
-const calcular_media = document.getElementsByName("calcular_media")
+let nota_1 = document.querySelector("nota_1")
+let nota_2 = document.querySelector("nota_2")
+let trab = document.querySelector("trab")
+const calcular_media = document.querySelector("calcular_media")
 // resultado da média
 let media = document.getElementById("media")
 let resultado = document.getElementById("resultado")
 // exame (caso média < 7)
-let nota_exame = document.getElementsByName("nota_exame")
-let trab_exame = document.getElementsByName("trab_exame")
-const calcular_exame = document.getElementsByName("calcular_exame")
+let nota_exame = document.querySelector("nota_exame")
+let trab_exame = document.querySelector("trab_exame")
+const calcular_exame = document.querySelector("calcular_exame")
 // resultado do exame
-let media_exame = document.getElementsByName("media-exame")
-let resultado_final = document.getElementsByName("resultado-final")
-
+let media_exame = document.querySelector("media-exame")
+let resultado_final = document.querySelector("resultado-final")
